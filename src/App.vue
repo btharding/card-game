a898e443a2a74f54bb1af5e75afe9abe477043eb
Billy Harding <btharding2014@hotmail.com>
2024-03-10 14:40:05 +0000

    Add card game

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <ResizablePane/>
+</template>
+
+<script>
+import ResizablePane from './components/ResizablePane.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ResizablePane
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Pathway+Gothic+One&display=swap');
+* {
+  box-sizing: border-box;
+  font-family: 'Pathway Gothic One', sans-serif;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
+#app {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/PlayingCard.vue
@@ -0,0 +1,129 @@
+<template>
+    <div @click="select" class="card" :class="{selected: selected}" :style="style">
+        <div class="card-content">
+            <div class="card-rear" :style="rearStyle">
+            </div>
+            <div class="card-detail" ref="cardDetail">
+                <p :style="txtStyle">{{ text }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PlayingCard",
+    directives: {
+    },
+    props: {
+        card: Object,
+        transformations: Object,
+        dismissedCardIds: Array,
+        players: Array
+    },
+    data: function () {
+        return {
+            selected: false,
+            text: '',
+            txtStyle: '',
+            rearStyle: '',
+        }
+    },
+    methods: {
+        select: function () {
+            if (this.selected) {
+                return;
+            }
+            this.text = this.formatText();
+            if (this.text.length > 200) {
+                this.txtStyle = 'font-size: 0.5rem';
+            } else if (this.text.length > 100) {
+                console.log('aaa');
+                this.txtStyle = 'font-size: 0.6rem';
+            } else {
+                this.txtStyle = '';
+            }
+            this.selected = true;
+            this.$emit('selected');
+        },
+        formatText: function () {
+            let wildCards = this.card.txt.match(/(\$\d+)/gmi);
+            
+            if (wildCards == null) {
+                return this.card.txt;
+            }
+            let cardText = this.card.txt;
+            let handledWildCards = [];
+            let availableNames = this.players;
+            for (let wildCard of wildCards) {
+                if (handledWildCards.includes(wildCard)) {
+                    continue;
+                }
+                handledWildCards.push(wildCard);
+                let replacement = availableNames[Math.floor(Math.random()*availableNames.length)];
+                availableNames = availableNames.filter((n) => {return n.id !== replacement.id});
+                cardText = cardText.replaceAll(wildCard, replacement.name);
+            }
+            return cardText;
+        }
+    },
+    computed: {
+        style () {
+            if (this.dismissedCardIds.filter((id) => {return id == this.card.id}).length) {
+                return {display: 'none'};
+            }
+            if (!this.selected) {
+                return { transform: 'translate(' + this.transformations.offsetX + 'px, ' + this.transformations.offsetY + 'px) rotate(' + this.transformations.rotation + 'deg)'}
+            }
+            return { transform: 'scale(3) translate(180px, 0px)'};
+        },  
+    }
+}
+</script>
+
+<style scoped>
+    p {
+        font-size: 0.75rem;
+    }
+    .card {
+        width: 80px;
+        height: 160px;
+        position: absolute;
+        top: calc(50%-80px);
+        left: 360px;
+        transition: all 0.8s;
+    }
+
+    .card-content {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        transition: transform 1.2s;
+        transform-style: preserve-3d;
+        text-align: center;
+    }
+
+    .card.selected .card-content {
+        transform: rotateY(-180deg);
+    }
+
+    .card-rear, .card-detail {
+        position: absolute;
+        width: 80px;
+        height: 160px;
+        -webkit-backface-visibility: hidden; /* Safari */
+        backface-visibility: hidden;
+    }
+
+    .card-rear {
+        border: 3px solid #271f41;
+        background-color: #000f09;
+    }
+
+    .card-detail {
+        background-color: #d0cfd7;
+        border: 3px solid #271f41;
+        color: #271f41;
+        transform: rotateY(-180deg);
+    }
+</style>
